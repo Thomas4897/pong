579a3884240c0a0d03ff732c918ba9b92b15db26
Thomas4897 <t.maynard4897@gmail.com>
2021-11-02 03:49:31 +0800

    Adding Notes

--- main.js
+++ main.js
@@ -30,16 +30,21 @@ ball.style.top = "250px";
 
 // Update the pong world
 function update() {
+	//! Add px velocity movement to the ball on y axis
 	ballPositionY = ballPositionY - ballVelocityY;
 	ball.style.top = ballPositionY + "px";
 
+	//! Add px velocity movement to the ball on x axis
 	ballPositionX = ballPositionX - ballVelocityX;
 	ball.style.left = ballPositionX + "px";
 
+	//! LeftSide:
+	//? If the ball position is 0 or less bounce the other direction
 	if (ballPositionX <= 0) {
 		console.log("ball hit left");
 		ballVelocityX = -Math.abs(ballVelocityX) - 0.5;
 
+		//? If the ball hits the left side and Y is 0 or less bounce the oppiste direction of up or down
 		if (ballVelocityY >= 0) {
 			ballVelocityY = Math.abs(ballVelocityY) + 0.5;
 		} else {
@@ -47,9 +52,13 @@ function update() {
 		}
 	}
 
+	//! RightSide:
+	//? If the ball position is 680 or more bounce the other direction
 	if (ballPositionX >= 680) {
 		console.log("ball hit right");
 		ballVelocityX = Math.abs(ballVelocityX) + 0.5;
+
+		//? If the ball hits the right side and Y is 0 or less bounce the oppiste direction of up or down
 		if (ballVelocityY >= 0) {
 			ballVelocityY = Math.abs(ballVelocityY) + 0.5;
 		} else {
@@ -57,12 +66,15 @@ function update() {
 		}
 	}
 
-	// If the ball hits the bottom of the screen, bounce up
+	//? Top:
+	//!If the ball hits the top of the screen, bounce down
 	if (ballPositionY <= 0) {
 		console.log("ball hit bottom");
 		ballVelocityY = -Math.abs(ballVelocityY) - 0.5;
 	}
 
+	//? Bottom:
+	//! If the ball hits the bottom of the screen, bounce up
 	if (ballPositionY >= 480) {
 		console.log("ball hit bottom");
 		ballVelocityY = Math.abs(ballVelocityY) + 0.5;
